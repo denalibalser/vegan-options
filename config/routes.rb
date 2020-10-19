9f1941794f8d566ca1276f6f6110655a34bf6bdf
denalibalser <denali.balser167@gmail.com>
2020-10-19 12:06:18 -0600

    worked on routes and added to views

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :reviews
   resources :restaurants
   resources :users
+  
+  get '/login' => 'users#new'
+  get '/signup' => 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
