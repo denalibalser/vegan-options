ca6094518a73d237cc63dc81ba74bd0d90afb3ba
denalibalser <denali.balser167@gmail.com>
2020-10-19 12:28:03 -0600

    added user new form and worked on  routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :restaurants
   resources :users
   
-  get '/login' => 'users#new'
-  get '/signup' => 'sessions#new'
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
