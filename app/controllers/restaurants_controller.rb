422b406093d3a116bbf29b3e25139b864e6dd369
denalibalser <denali.balser167@gmail.com>
2020-10-20 10:41:37 -0600

    added restaurant count to state index and added new form and action  for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,20 @@
 class RestaurantsController < ApplicationController
+    def new 
+        @restaurant = Restaurant.new 
+    end 
+
+    def create 
+        @restaurant = Restaurant.create(restaurant_params)
+
+        redirect_to restaurant_path(@restaurant)
+    end 
+
+    def show  
+    end 
+
+    private  
+
+    def restaurant_params  
+    end 
     
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   belongs_to :state
-  has_many: reviews 
-  has_many: users, through: reviews
+  has_many :reviews 
+  has_many :users, through: :reviews
 end
