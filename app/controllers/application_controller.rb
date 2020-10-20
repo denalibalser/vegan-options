4a5317abc6d9f17db999815985da4b9f88e5d11d
denalibalser <denali.balser167@gmail.com>
2020-10-20 13:04:14 -0600

    assigned user_id of current user to restaurant when created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    helper_method :current_user
+    helper_method :logged_in?
+
+    def current_user
+        if session[:user_id]
+          User.find(session[:user_id])
+        end
+      end
+    
+      def logged_in?
+        !!current_user
+      end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,14 @@
 class RestaurantsController < ApplicationController
+    require 'pry'
     def new 
         @restaurant = Restaurant.new 
     end 
 
     def create 
         @restaurant = Restaurant.new(restaurant_params)
+        @restaurant.user_id = current_user.id  #may need to work on this --- but is working now i think
+
+        binding.pry
         @restaurant.save
 
         redirect_to restaurant_path(@restaurant)
@@ -17,7 +21,7 @@ class RestaurantsController < ApplicationController
     private  
 
     def restaurant_params
-        params.require(:restaurant).permit(:name, :description, :address, :phone_number, :vegan_friendliness_rating, :price_rating, :state_id)  
+        params.require(:restaurant).permit(:name, :description, :address, :phone_number, :vegan_friendliness_rating, :price_rating, :user_id, :state_id)  
     end 
     
 end
