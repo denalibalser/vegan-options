04dd45b2689508ac1b18176e302bcaa78c8b6f85
denalibalser <denali.balser167@gmail.com>
2020-10-20 12:30:50 -0600

    updated gemfile for bcrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(username: params[:user][:username]) 
-        if @user && @user.authenticate(params[:user][:password_digest])
+        if @user && @user.authenticate(params[:user][:password]) #getting error here 
             session[:user_id] = @user.id
             
 
