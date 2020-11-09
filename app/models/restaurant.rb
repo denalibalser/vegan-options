3d3d85e84f282f8ce241b6bb5fe79496cb4a747f
denalibalser <denali.balser167@gmail.com>
2020-11-09 14:42:07 -0700

    added scope method to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,9 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy 
   has_many :users, through: :reviews
 
+  scope :sort_alphabetical, ->{order(:name)}
+
+
   validates :name, :description, :vegan_friendliness_rating, :state_id, :phone_number, presence: true, allow_blank: false
   validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "incorrect format, correct example: 234-567-8900" }
 
